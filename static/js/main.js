a2ac9923537af1ee557f242956279baa8bb5c868
Zaid Hakak <zaidaijaz@gmail.com>
2017-01-19 19:58:29 +0530

    Fixed marginal issue

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,8 @@
 $(document).ready(function () {
     $("#owl-demo").owlCarousel({
 
-        autoplay: 1000, //Set AutoPlay to 3 seconds
+        autoplay: 1000,
+        loop: true,
         items: 2,
         dots: false,
         itemsDesktop: [1199, 2],
