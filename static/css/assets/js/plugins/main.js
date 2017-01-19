2ad7de14ef716e0aa311a91ed0b8ea7ed02a26c3
Zaid Hakak <zaidaijaz@gmail.com>
2017-01-19 17:31:17 +0530

    CleanUp

--- static/css/assets/js/plugins/main.js
+++ static/css/assets/js/plugins/main.js
@@ -3,8 +3,11 @@
 $(document).ready(function () {
     $("#owl-demo").owlCarousel({
 
-        autoPlay: 2000, //Set AutoPlay to 3 seconds
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
         items: 2,
+        dots:false,
+        autoWidth:true,
+
 
     });
 });
